b0e93adcccc77484985ac9fd9f4bf6d0142de7d7
ivpriyono <iv.priyono@gmail.com>
2024-05-04 17:59:59 +0700

    Add main class to master.php

--- app/Views/layouts/master.php
+++ app/Views/layouts/master.php
@@ -2,23 +2,23 @@
 <html lang="en">
 
 <head>
-    <?= $this->include('layouts/partials/head') ?>
-    <?= $this->renderSection('style') ?>
+    <?= $this->include("layouts/partials/head") ?>
+    <?= $this->renderSection("style") ?>
 </head>
 
 <body class="bg-body-secondary">
     <div class="container-fluid">
         <div class="row">
-            <?= $this->include('layouts/partials/sidebar') ?>
-            <main class="col-md-9 ml-sm-auto col-lg-10 p-md-4">
-                <?= $this->include('layouts/partials/topbar') ?>
-                <?= $this->renderSection('content') ?>
+            <?= $this->include("layouts/partials/sidebar") ?>
+            <main class="col-md-9 ml-sm-auto col-lg-10 p-md-4 main">
+                <?= $this->include("layouts/partials/topbar") ?>
+                <?= $this->renderSection("content") ?>
             </main>
         </div>
     </div>
 
-    <?= $this->include('layouts/partials/script') ?>
-    <?= $this->renderSection('script') ?>
+    <?= $this->include("layouts/partials/script") ?>
+    <?= $this->renderSection("script") ?>
 </body>
 
 </html>
