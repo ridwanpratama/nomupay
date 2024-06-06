226f66e59c76b401a0a87746a929db55c1ade328
faizirrohman <faizirrohman>
2024-06-06 16:55:42 +0700

    get balance di menu send

--- app/Controllers/SendController.php
+++ app/Controllers/SendController.php
@@ -2,11 +2,16 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Services\TransactionService;
 
 class SendController extends BaseController
 {
     public function index ()
     {
-        return view('send/index');
+        $transactionService     = new TransactionService();
+
+        $userBalance            = $transactionService->getUserBalance(session('id'));
+
+        return view('send/index', compact('userBalance'));
     }
 }
