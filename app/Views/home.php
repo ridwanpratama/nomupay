1526ba332e39951815db12a5a724ac0981dd6b51
ridwan <pratamaridwan111@gmail.com>
2024-06-11 08:32:23 +0700

    fix url

--- app/Views/home.php
+++ app/Views/home.php
@@ -45,8 +45,7 @@
                             <a class="nav-link" href="https://wa.me/6285710386771">Contact</a>
                         </li>
                     </ul>
-                    <a href="<?= base_url() ?><?php if (session()->get("isLoggedIn")): ?>mypanel/dashboard<?php else: ?>auth/register<?php endif; ?>" class="btn btn-primary ml-lg-3 primary-shadow"><?= session()->get("isLoggedIn") ? "Dashboard" : "Sign Up" ?>
-                    </a>
+                    <a href="<?= base_url() . (session()->get("isLoggedIn") ? 'mypanel/dashboard' : 'auth/register') ?>" class="btn btn-primary ml-lg-3 primary-shadow"><?= session()->get("isLoggedIn") ? "Dashboard" : "Sign Up" ?>
                 </div>
             </nav>
         </div>
